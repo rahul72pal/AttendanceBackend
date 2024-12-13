74dae8dc34a657cedd4d54a09c0fa4cd2fba9b74
Rahul <rahulgwl72@gmail.com>
2024-12-13 22:21:38 +0530

    add only "*"

--- app.js
+++ app.js
@@ -17,8 +17,15 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://students-nine-iota.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 app.use(cors({
-  origin: ["https://students-nine-iota.vercel.app","http://localhost:5173"],
+  origin:"*",
   credentials: true,
 }))
 
